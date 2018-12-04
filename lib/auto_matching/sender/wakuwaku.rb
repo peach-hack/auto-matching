b38618ffc38d4e00c4d4033fcdc594f74a9403f7
tsu-nera <fox10225fox@gmail.com>
2018-12-04 15:56:11 +0900

    ワクワク投稿 done

--- lib/auto_matching/sender/wakuwaku.rb
+++ lib/auto_matching/sender/wakuwaku.rb
@@ -20,13 +20,24 @@ module AutoMatching
 
         def delete_past_post
           session.visit("http://550909.com/m/bbs/history")
-          session.execute_script "document.post.all.value=1;document.post.submit();return false;"
-          session.execute_script "document.post.submit();return false;"
+
+          # すでに投稿がある場合は投稿を削除
+          unless session.has_css?(".BtnToPureBBS")
+            session.execute_script "document.post.all.value=1;document.post.submit();return false;"
+            session.execute_script "document.post.submit();return false;"
+          end
         end
 
         def send_new_post
-        end
+          session.visit("http://550909.com/m/bbs/post/?menu=adult&genre=3")
 
+          session.fill_in "title", with: post[:title]
+          session.fill_in "body", with: post[:body]
+
+          unless ENV["DEBUG"]
+            session.execute_script "$('.js-btn--post').trigger('click')"
+          end
+        end
     end
   end
 end
