9c13c89ca5ac09d433a4fc4485e0b4ac982c7158
tsu-nera <fox10225fox@gmail.com>
2018-12-06 12:16:26 +0900

    自動投稿 mint done

--- lib/auto_matching/sender/mint.rb
+++ lib/auto_matching/sender/mint.rb
@@ -15,14 +15,23 @@ module AutoMatching
           session.fill_in "loginid", with: login_user
           session.fill_in "pwd", with: login_password
           session.find("#B1login").click
-
-          sleep 3
         end
 
         def delete_past_post
+          session.visit("https://mintj.com/ms/mb/MB.aspx?sid=&pf=7")
+          session.click_link("③投稿一覧")
+          session.first("#Bdel").click
         end
 
         def send_new_post
+          session.visit("https://mintj.com/ms/mb/Wr.aspx?sid=&cgid=1&m=ad1")
+
+          session.fill_in "TBt", with: post[:title]
+          session.fill_in "TBm", with: post[:body]
+
+          unless ENV["DEBUG"]
+            session.find("#Bw").native.send_keys :enter
+          end
         end
     end
   end
