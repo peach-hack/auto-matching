76083e7f7bfa70afe037f1de9b754d47530999e4
tsu-nera <fox10225fox@gmail.com>
2018-12-06 16:16:02 +0900

    merupara base

--- app/models/source_site.rb
+++ app/models/source_site.rb
@@ -25,6 +25,7 @@ class SourceSite < ApplicationRecord
   KEY_PCMAX = "pcmax".freeze
   KEY_IKUKURU = "ikukuru".freeze
   KEY_MINT = "mint".freeze
+  KEY_MERUPARA = "merupara".freeze
 
   validates :key, presence: true, uniqueness: true
   validates :name, presence: true
---
+++ lib/auto_matching/sender/merupara.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module AutoMatching
+  module Sender
+    class Mint < PostSenderBase
+      class << self
+        def source_site_key
+          SourceSite::KEY_MERUPARA
+        end
+      end
+
+      private
+
+        def try_login
+        end
+
+        def delete_past_post
+        end
+
+        def send_new_post
+        end
+      
+    end
+  end
+end
