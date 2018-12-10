22f880b265f51b6ff2a29b74c8892eb445759ee3
tsu-nera <fox10225fox@gmail.com>
2018-12-10 12:24:05 +0900

    全部消す

--- app/jobs/auto_posting_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AutoPostingJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    rake "daily_post:all"
-  end
-end
--- config/initializers/locale.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-I18n.config.available_locales = :ja
-I18n.default_locale = :ja
