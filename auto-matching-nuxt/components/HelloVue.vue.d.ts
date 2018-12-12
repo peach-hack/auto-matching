a2d7248a44ae709f520abcb77fb3672364c11db8
tsu-nera <fox10225fox@gmail.com>
2018-12-12 21:39:13 +0900

    起動

--- auto-matching-nuxt/components/HelloVue.vue.d.ts
+++ auto-matching-nuxt/components/HelloVue.vue.d.ts
@@ -1,13 +1,11 @@
 import { Vue } from "nuxt-property-decorator";
-export default class Hello extends Vue {
+export default class HelloVue extends Vue {
     val: string;
     value: string;
-    5: any;
     inputValue: string;
     clickButton(val: string): void;
     onValueChange(newValue: string, oldValue: string): void;
     readonly isDisabled: boolean;
-    mounted(): void;
     handleInput($event: Event): void;
     handleClick(): void;
 }
