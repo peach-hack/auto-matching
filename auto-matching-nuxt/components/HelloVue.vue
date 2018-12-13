76d113152f03ac4b31c65e094547dd673e93cdf0
tsu-nera <fox10225fox@gmail.com>
2018-12-13 20:48:03 +0900

    エラー除去

--- auto-matching-nuxt/components/HelloVue.vue
+++ auto-matching-nuxt/components/HelloVue.vue
@@ -16,9 +16,7 @@
 </template>
 
 <script lang="ts">
-import {
-  Component, Prop, Watch, Vue,
-} from 'nuxt-property-decorator';
+import { Component, Prop, Watch, Vue } from "nuxt-property-decorator";
 
 @Component({
   /** filters */
@@ -28,27 +26,28 @@ import {
         return null;
       }
       return value.toUpperCase();
-    },
-  },
+    }
+  }
 })
 export default class HelloVue extends Vue {
   /** props */
   @Prop()
-  val: string;
+  val: string = "";
 
   /** data */
   value: string = this.val;
 
-  inputValue: string = '';
+  inputValue: string = "";
 
   /** watch */
-  @Watch('value') onValueChange(newValue: string, oldValue: string): void {
+  @Watch("value")
+  onValueChange(newValue: string, oldValue: string): void {
     console.log(`watch: ${newValue}, ${oldValue}`);
   }
 
   /** computed */
   get isDisabled(): boolean {
-    return this.inputValue === '';
+    return this.inputValue === "";
   }
 
   /** methods */
@@ -57,12 +56,12 @@ export default class HelloVue extends Vue {
   }
 
   handleClick(): void {
-    if (this.inputValue === '') {
+    if (this.inputValue === "") {
       return;
     }
     this.value = this.inputValue;
-    this.inputValue = '';
-    this.$emit('handle-click', this.value);
+    this.inputValue = "";
+    this.$emit("handle-click", this.value);
   }
 }
 </script>
