0bf3c58cd5eea14ffbbb9b32ee675a0da3c52aac
tsu-nera <fox10225fox@gmail.com>
2018-12-15 07:32:18 +0900

    はじめてのRSpec

---
+++ auto-matching-api-rails/spec/requests/users/source_sites_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Users::SourceSitesController, type: :request do
+  let!(:source_sites) { 6.times { create(:source_site) } }
+
+  describe "#index" do
+    subject { get users_source_sites_path }
+
+    before do
+      subject
+    end
+
+    context "正常系" do
+      it "レスポンスステータスが200であること" do
+        expect(response).to have_http_status(200)
+        expect(json.length).to eq(SourceSite.count)
+      end
+    end
+  end
+end
