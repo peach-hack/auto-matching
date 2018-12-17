335c04594c938177dc4ee99fc4dff3b0a9d5a6a4
tsu-nera <fox10225fox@gmail.com>
2018-12-17 18:34:00 +0900

    eslint指摘修正

--- auto-matching-nuxt/pages/users/sites/index.vue
+++ auto-matching-nuxt/pages/users/sites/index.vue
@@ -1,38 +1,35 @@
 <template>
   <div>
     <h2>サイト一覧</h2>
-    <div v-for="site in sites">
+    <div v-for="site in sites" :key="site.id">
       <div slot="header">
-        <span>{{site.name}}</span>
+        <span>{{ site.name }}</span>
       </div>
-      <div >
-        <p>{{site.url}}</p>
+      <div>
+        <p>{{ site.url }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import axios from 'axios';
+import { Vue, Component } from "vue-property-decorator";
+import axios from "axios";
 
 @Component
 export default class Reviews extends Vue {
-    sites: string[] = [];
+  sites: string[] = [];
 
-    async mounted(): Promise<void> {
-        axios.defaults.baseURL = 'http://localhost:5000';
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-        await this.fetchAquariumReviews();
-    }
+  async mounted(): Promise<void> {
+    axios.defaults.baseURL = "http://localhost:5000";
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    await this.fetchAquariumReviews();
+  }
 
-    async fetchAquariumReviews(): Promise<void> {
-        await axios.get(`api/users/source-sites`).then((response) => {
-            console.log(response);
-            response.data.map((site: any) => {
-                this.sites.push(site);
-            })
-        })
-    }
+  async fetchAquariumReviews(): Promise<void> {
+    await axios.get(`api/users/source-sites`).then(response => {
+      response.data.map((site: any) => this.sites.push(site));
+    });
+  }
 }
 </script>
