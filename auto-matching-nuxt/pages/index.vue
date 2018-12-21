30d62db1c68ca706d2664241db8bf6402152025d
tsu-nera <fox10225fox@gmail.com>
2018-12-22 00:26:42 +0900

    styled-componentsの導入

--- auto-matching-nuxt/pages/index.vue
+++ auto-matching-nuxt/pages/index.vue
@@ -1,33 +1,26 @@
 <template lang="pug">
-section.container
+styled-container
   div
     logo
-    h1.title
+    styled-title
       | Auto Matching
-    h2.subtitle
+    styled-subtitle
       | Dating meets Tech
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import styled from 'vue-styled-components'
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
+const StyledContainer = styled.section`
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
+`
 
-.title {
+const StyledTitle = styled.h1`
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
@@ -35,13 +28,22 @@ export default {
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
-}
+`
 
-.subtitle {
+const StyledSubtitle = styled.h2`
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+`
+
+export default {
+  components: {
+    Logo,
+    StyledContainer,
+    StyledTitle,
+    StyledSubtitle
+  }
 }
-</style>
+</script>
