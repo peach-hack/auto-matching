943094eb21fa615c55ee6afc4fa820e034d44c72
tsu-nera <fox10225fox@gmail.com>
2018-12-21 16:34:38 +0900

    api responseaを返すように修正

--- auto-matching-api-rails/app/controllers/api/users/source_sites_controller.rb
+++ auto-matching-api-rails/app/controllers/api/users/source_sites_controller.rb
@@ -5,6 +5,19 @@ module Api
         source_sites = SourceSite.all
         render json: SourceSiteSerializer.new(source_sites).serialized_json
       end
+
+      def update
+        source_site = SourceSite.find(params[:id])
+
+        source_site.login_user = params[:login_user]
+        source_site.login_password = params[:login_password]
+
+        if source_site.save
+          response_success(:source_site, :update)
+        else
+          response_bad_request
+        end
+      end
     end
   end
 end
