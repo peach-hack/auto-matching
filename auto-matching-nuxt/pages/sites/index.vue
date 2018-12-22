c4eba99ccdc24ccf71118e65ddba9462eda7949e
tsu-nera <fox10225fox@gmail.com>
2018-12-22 11:02:06 +0900

    eslint対応

--- auto-matching-nuxt/pages/sites/index.vue
+++ auto-matching-nuxt/pages/sites/index.vue
@@ -23,20 +23,22 @@ b-card
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator'
-  import axios from 'axios'
-  @Component
-  export default class Sites extends Vue {
-    sites: string[] = []
-    async mounted(): Promise<void> {
-      axios.defaults.baseURL = 'http://localhost:5000'
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-      await this.getApiUsersSourceSites()
-    }
-    async getApiUsersSourceSites(): Promise<void> {
-      await axios.get('api/users/source-sites').then(response => {
-        response.data.data.map((site: any) => this.sites.push(site))
-      })
-    }
+import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
+@Component
+export default class Sites extends Vue {
+  sites: string[] = []
+
+  async mounted(): Promise<void> {
+    axios.defaults.baseURL = 'http://localhost:5000'
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+    await this.getApiUsersSourceSites()
   }
+
+  async getApiUsersSourceSites(): Promise<void> {
+    await axios.get('api/users/source-sites').then(response => {
+      response.data.data.map((site: any) => this.sites.push(site))
+    })
+  }
+}
 </script>
