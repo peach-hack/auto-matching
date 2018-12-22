45c67306870b936a87255abffbf0f8ab4e97fb75
tsu-nera <fox10225fox@gmail.com>
2018-12-22 21:00:22 +0900

    フォーム骨格

--- auto-matching-nuxt/pages/sites/_id.vue
+++ auto-matching-nuxt/pages/sites/_id.vue
@@ -1,4 +1,23 @@
 <template lang="pug">
 b-card
-  p test
+  form
+    .form-group
+      label ログインID
+      input.form-control(type="text" placeholder="LoginId")
+    .form-group
+      label ログインパスワード
+      input.form-control(type="text" placeholder="Password")
+    fieldset.form-group
+      .row
+        legend.col-form-label.col-sm-2.pt-0
+          | 操作対象
+        .col-sm-10
+          .form-check
+            input.form-check-input(type="radio" name="activate" value="option1" checked)
+            | 有効
+          .form-check
+            input.form-check-input(type="radio" name="deactivate" value="option1")
+            | 無効
+    button(type="submit").btn.btn-primary.mb-2
+      | Submit
 </template>
