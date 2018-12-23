043009f1622194fe68fb26faceb709985349b8d9
tsu-nera <fox10225fox@gmail.com>
2018-12-24 05:13:20 +0900

    add procfile

--- auto-matching-nuxt/nuxt.config.js
+++ auto-matching-nuxt/nuxt.config.js
@@ -1,14 +1,6 @@
 const extendConfig = require('./webpack.config.extend')
 const pkg = require('./package.json')
 
-const env = process.env.NODE_ENV || 'development'
-let API_URL
-if (env === 'development' || env === 'test') {
-  API_URL = 'http://localhost:5000'
-} else {
-  API_URL = process.env.API_URL
-}
-
 module.exports = {
   mode: 'spa',
 
