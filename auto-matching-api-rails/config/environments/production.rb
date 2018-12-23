e3cec4b8dad367d46e943c514fa361b37bee9ee0
tsu-nera <fox10225fox@gmail.com>
2018-12-24 00:20:16 +0900

    disable assets

--- auto-matching-api-rails/config/environments/production.rb
+++ auto-matching-api-rails/config/environments/production.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.enabled = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
