d630b738d3c5564e3572cef8ecca6fd48a5aa36f
tsu-nera <fox10225fox@gmail.com>
2018-12-24 02:44:34 +0900

    typo

--- auto-matching-api-rails/db/fixtures/post.rb
+++ auto-matching-api-rails/db/fixtures/post.rb
@@ -2,7 +2,13 @@
 
 require "yaml"
 
-posts = YAML.load_file("db/templates/post.yml") || []
+post_file = "db/templates/post.yml"
+
+if File.exist?(post_file)
+  posts = YAML.load_file(post_file)
+else
+  posts = []
+end
 
 posts.each_with_index do |post, i|
   Post.seed do |p|
