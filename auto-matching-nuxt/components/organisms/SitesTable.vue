e7665eb309f0a0a8547efd687252fad490f38b11
tsu-nera <fox10225fox@gmail.com>
2018-12-25 20:35:10 +0900

    refactoring

---
+++ auto-matching-nuxt/components/organisms/SitesTable.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+table.table
+  thead
+    tr
+      th #
+      th サイト名
+      th ログインID
+      th ログインパスワード
+      th 操作対象
+      th 編集
+  tbody
+    tr(v-for="site in sites" :key="site.id")
+      th {{ site.attributes.id }}
+      th(v-html="site.attributes.link")
+      td {{ site.attributes.login_user }}
+      td {{ site.attributes.login_password }}
+      td {{ site.attributes.activate_flag }}
+      td
+        nuxt-link(:to="{ path: '/sites/' + site.id }") 編集
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    sites: Object
+  }
+}
+</script>
--- auto-matching-nuxt/pages/sites/index.vue
+++ auto-matching-nuxt/pages/sites/index.vue
@@ -1,30 +1,17 @@
 <template lang="pug">
 b-card
-  table.table
-    thead
-      tr
-        th #
-        th サイト名
-        th ログインID
-        th ログインパスワード
-        th 操作対象
-        th 編集
-    tbody
-      tr(v-for="site in sites" :key="site.id")
-        th {{ site.attributes.id }}
-        th(v-html="site.attributes.link")
-        td {{ site.attributes.login_user }}
-        td {{ site.attributes.login_password }}
-        td {{ site.attributes.activate_flag }}
-        td
-          nuxt-link(:to="{ path: '/sites/' + site.id }") 編集
+  sites-table(:sites="sites")
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import SitesTable from '~/components/organisms/SitesTable'
 import { getApiUsersSourceSites } from '~/plugins/api'
 
 export default Vue.extend({
+  components: {
+    SitesTable
+  },
   data() {
     return {
       sites: [] as any
