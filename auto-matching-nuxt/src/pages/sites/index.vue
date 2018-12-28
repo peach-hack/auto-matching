315928f9487d718b26d20330871372e10320b20c
tsu-nera <fox10225fox@gmail.com>
2018-12-29 07:01:32 +0900

    wip

--- auto-matching-nuxt/src/pages/sites/index.vue
+++ auto-matching-nuxt/src/pages/sites/index.vue
@@ -3,7 +3,7 @@ sites-table(:sites="sites")
 </template>
 
 <script lang="ts">
-import Vue, { ComponentOptions } from 'vue'
+import Vue from 'vue'
 import SitesTable from '@/components/organisms/SitesTable.vue'
 
 const Api = require('@/plugins/api')
@@ -22,5 +22,5 @@ export default Vue.extend({
       response.data.data.map((site: any) => this.sites.push(site))
     })
   }
-} as ComponentOptions<Vue>)
+} as any)
 </script>
