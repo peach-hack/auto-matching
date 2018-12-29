3cf7f139a5f4e3f892ab05cf7b65e2f5c496737b
tsu-nera <fox10225fox@gmail.com>
2018-12-30 00:42:36 +0900

    error fix

--- auto-matching-nuxt/src/pages/sites/index.vue
+++ auto-matching-nuxt/src/pages/sites/index.vue
@@ -6,6 +6,8 @@ sites-table(:sites="sites")
 import Vue from 'vue'
 import SitesTable from '@/components/organisms/SitesTable.vue'
 
+const Api = require('@/plugins/api')
+
 export default Vue.extend({
   components: {
     SitesTable
@@ -16,7 +18,7 @@ export default Vue.extend({
     }
   },
   mounted() {
-    this.getApiUsersSourceSites().then((response: any) => {
+    Api.getApiUsersSourceSites().then((response: any) => {
       response.data.data.map((site: any) => this.sites.push(site))
     })
   }
