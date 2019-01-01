703d932e874e32bad85d584908a6df9919a708e8
tsu-nera <fox10225fox@gmail.com>
2019-01-02 02:24:16 +0900

    fix error

--- auto-matching-nuxt/src/pages/sites/_id.vue
+++ auto-matching-nuxt/src/pages/sites/_id.vue
@@ -1,24 +1,24 @@
 <template lang="pug">
 div
   // h2 {{ getSite(siteId).name }}
-  site-form(:id=id)
+  site-form(:id="id")
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapGetters } from 'vuex'
 import SiteForm from '@/components/organisms/SiteForm.vue'
 
 const Api = require('@/plugins/api')
 
+// @ts-ignore
 export default Vue.extend({
   components: {
     SiteForm
   },
   computed: {
-    id: function({ route }: { route: any }): number {
-      return parseInt(route.params.id)
+    id: function(): number {
+      return parseInt(this.$route.params.id)
     }
   }
-} as any)
+})
 </script>
