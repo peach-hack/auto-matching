b1fc8e9479d20af4a5efda23b43e55c6437eb127
tsu-nera <fox10225fox@gmail.com>
2019-01-04 04:00:06 +0900

    いまのうちにcommit

---
+++ auto-matching-nuxt/src/pages/posts/templates/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+sites-table
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SitesTable from '@/components/organisms/SitesTable.vue'
+
+export default Vue.extend({
+  components: {
+    SitesTable
+  },
+  async fetch({ store }: { store: any }) {
+    await store.dispatch('sites/fetchSites')
+  }
+} as any)
+</script>
