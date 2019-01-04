aa50da805cb1270d068e897d088d60a61225087a
tsu-nera <fox10225fox@gmail.com>
2019-01-05 04:33:09 +0900

    rails側をつくるのでいったんcommit

--- auto-matching-nuxt/src/components/organisms/TemplateForm.vue
+++ auto-matching-nuxt/src/components/organisms/TemplateForm.vue
@@ -2,10 +2,10 @@
 form(@submit.prevent="updateTemplate")
   .form-group
     label タイトル
-    input.form-control(type="text" v-model="template.title")
+    input.form-control(type="text" v-model="newTitle")
   .form-group
     label 本文
-    textarea.form-control(type="text" v-model="template.body")
+    textarea.form-control(type="text" v-model="newBody")
   submit-button-group(path="/posts/templates")
 </template>
 
@@ -26,6 +26,12 @@ export default Vue.extend({
       required: true
     }
   },
+  data() {
+    return {
+      newTitle: this.template.title as string,
+      newBody: this.template.body as string
+    }
+  },
   methods: {
     updateTemplate: function() {
       putApiUsersPostsTemplatesById({
@@ -33,6 +39,10 @@ export default Vue.extend({
         attributes: this.template
       })
         .then((response: any) => {
+          this.$store.commit('addTemplate', response.data.data)
+          this.newTitle = ''
+          this.newBody = ''
+
           this.$router.push('/posts/templates')
           this.$toasted.success('更新しました')
         })
