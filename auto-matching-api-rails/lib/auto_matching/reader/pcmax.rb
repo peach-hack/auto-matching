4bc2059307b63c6eb9dbabc60b75588a649da738
tsu-nera <fox10225fox@gmail.com>
2019-01-06 17:24:42 +0900

    wip

--- auto-matching-api-rails/lib/auto_matching/reader/pcmax.rb
+++ auto-matching-api-rails/lib/auto_matching/reader/pcmax.rb
@@ -7,6 +7,22 @@ module AutoMatching
     private
       def read_board
         # TODO
+
+        @list = []
+
+        @data = {}
+        @data[:name] = hoge
+        @data[:age] = 30
+
+        @list.add(@data)
+
+      end
+
+      def save_board
+        @list.each do | data |
+          a = Post.new(name: data[:name], age: data[:age])
+          a.save
+        end
       end
   end
 end
