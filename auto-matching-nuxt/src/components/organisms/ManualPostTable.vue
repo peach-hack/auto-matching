317e16c4915bc6c46f3d5a618123a7d8d601ec6d
tsu-nera <fox10225fox@gmail.com>
2019-01-09 18:53:18 +0900

    checkbox disabled

--- auto-matching-nuxt/src/components/organisms/ManualPostTable.vue
+++ auto-matching-nuxt/src/components/organisms/ManualPostTable.vue
@@ -14,7 +14,7 @@ form(@submit.prevent="manualPost")
         tr(v-for="site in $state.sites.sites" :key="site.id")
           th
             .form-check
-              input.form-check-input.position-static(type="checkbox")
+              input.form-check-input.position-static(type="checkbox" :disabled="isCheckBoxDisabled(site)")
           th(v-html="getLink(site)")
           th {{ getActivate(site) }}
           th -
@@ -35,6 +35,9 @@ export default Vue.extend({
     },
     getActivate: function(site: Site): string {
       return site.activateFlag ? '有効' : '無効'
+    },
+    isCheckBoxDisabled: function(site: Site): boolean {
+      return !site.activateFlag
     }
   }
 })
