6fc58094c954820d0600ba2163964db1c37d9e04
tsu-nera <fox10225fox@gmail.com>
2019-01-09 23:12:23 +0900

    add foreign key

--- auto-matching-api-rails/app/serializers/source_site_serializer.rb
+++ auto-matching-api-rails/app/serializers/source_site_serializer.rb
@@ -4,18 +4,20 @@
 #
 #  id                          :bigint(8)        not null, primary key
 #  activate_flag(操作対象)         :boolean          not null
+#  affiliate_url(アフィリエイトURL)   :string           not null
 #  key(ユニークキー)                 :string           not null
 #  login_password(ログインパスワード情報) :string
+#  login_url(クロール開始URL)        :string           not null
 #  login_user(ログインユーザー情報)      :string
 #  name(サイト名)                  :string           not null
-#  public_url(サイトURL)          :string           not null
-#  url(クロール開始URL)              :string           not null
 #  created_at                  :datetime         not null
 #  updated_at                  :datetime         not null
+#  post_id                     :bigint(8)
 #
 # Indexes
 #
-#  index_source_sites_on_key  (key) UNIQUE
+#  index_source_sites_on_key      (key) UNIQUE
+#  index_source_sites_on_post_id  (post_id)
 #
 
 class SourceSiteSerializer < ApplicationSerializer
