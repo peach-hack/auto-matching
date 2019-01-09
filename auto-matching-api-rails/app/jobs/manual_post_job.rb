4bdcfebf2cac105f391fbc7a97a6596271091d63
tsu-nera <fox10225fox@gmail.com>
2019-01-10 03:54:08 +0900

    sidekiq動いた

--- auto-matching-api-rails/app/jobs/manual_post_job.rb
+++ auto-matching-api-rails/app/jobs/manual_post_job.rb
@@ -1,19 +1,19 @@
 class ManualPostJob < ApplicationJob
   queue_as :default
 
-  sender_classes = [
-    AutoMatching::Sender::Happymail,
-    AutoMatching::Sender::Wakuwaku,
-    AutoMatching::Sender::Pcmax,
-    AutoMatching::Sender::Ikukuru,
-    AutoMatching::Sender::Mint,
-    AutoMatching::Sender::Merupara,
-  ]
-
   def perform(site_ids, debug_flag)
+    sender_classes = [
+      AutoMatching::Sender::Happymail,
+      AutoMatching::Sender::Wakuwaku,
+      AutoMatching::Sender::Pcmax,
+      AutoMatching::Sender::Ikukuru,
+      AutoMatching::Sender::Mint,
+      AutoMatching::Sender::Merupara,
+    ]
+
     site_ids.each do |id|
       $DEBUG = debug_flag
-      AutoMatching::Sender::Executor.new.run(sender_classes[id])
+      AutoMatching::Sender::Executor.new.run(sender_classes[id - 1])
       $DEBUG = false
     end
   end
--- bin/install.sh
+++ bin/install.sh
@@ -3,5 +3,5 @@
 source bin/common.sh
 
 docker-compose run rails bundle
+docker-compose run worker bundle
 docker-compose run nuxt yarn
-
