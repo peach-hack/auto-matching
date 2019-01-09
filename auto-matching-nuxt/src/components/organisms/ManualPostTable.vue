98294fe7535e5941fa787098a2e796faec6234d8
tsu-nera <fox10225fox@gmail.com>
2019-01-09 18:32:31 +0900

    サイト一覧表示

---
+++ auto-matching-nuxt/src/components/organisms/ManualPostTable.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+form(@submit.prevent="manualPost")
+  .form-group
+    table.table
+      thead
+        tr
+          th
+            .form-check
+              input.form-check-input.position-static(type="checkbox")
+          th サイト名
+          th 操作対象
+          th 状態
+      tbody
+        tr(v-for="site in $state.sites.sites" :key="site.id")
+          th
+            .form-check
+              input.form-check-input.position-static(type="checkbox")
+          th(v-html="getLink(site)")
+          th {{ getActivate(site) }}
+          th -
+  .form-group        
+    button(type="button submit").btn.btn-primary
+      | 一括投稿
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+//@ts-ignore
+import Site from '@/types/site'
+
+export default Vue.extend({
+  methods: {
+    getLink: function(site: Site): string {
+      return `<a href=${site.url} target="_blank">${site.name}</a>`
+    },
+    getActivate: function(site: Site): string {
+      return site.activateFlag ? '有効' : '無効'
+    }
+  }
+})
+</script>
---
+++ auto-matching-nuxt/src/pages/posts/manual/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+manual-post-table
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+//@ts-ignore
+import ManualPostTable from '@/components/organisms/ManualPostTable.vue'
+
+export default Vue.extend({
+  components: {
+    ManualPostTable
+  },
+  async fetch({ store }: { store: any }) {
+    await store.dispatch('sites/fetchSites')
+  }
+} as any)
+</script>
