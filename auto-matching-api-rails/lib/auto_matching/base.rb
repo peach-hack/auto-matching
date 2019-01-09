86b6199b4bfb7141f3b3a41d00ad939f15063488
tsu-nera <fox10225fox@gmail.com>
2019-01-09 16:20:04 +0900

    add cookie

--- auto-matching-api-rails/lib/auto_matching/base.rb
+++ auto-matching-api-rails/lib/auto_matching/base.rb
@@ -7,7 +7,11 @@ module AutoMatching
     end
 
     def run
+      set_cookie
+
       run_process
+
+      save_cookie
     rescue StandardError => e
       save_current_page
       logger.error("#{e.message}")
@@ -68,11 +72,11 @@ module AutoMatching
       end
 
       def screenshot_name
-        Rails.root.join("tmp", "capybara", "#{ts}_screenshot.png")
+        Rails.root.join("tmp", "capybara", "#{ts}_#{source_site_key}_screenshot.png")
       end
 
       def html_name
-        Rails.root.join("tmp", "capybara", "#{ts}_page.html")
+        Rails.root.join("tmp", "capybara", "#{ts}_#{source_site_key}_page.html")
       end
 
       def ts
@@ -85,6 +89,33 @@ module AutoMatching
 
       def logger_name
         Rails.root.join("log", "execution.log")
+      end
+
+      def cookie_file_name
+        Rails.root.join("tmp", "cookie", "#{source_site_key}_cookie.json")
+      end
+
+      def load_cookie
+        return unless File.exist?(cookie_file_name)
+        File.open(cookie_file_name) do |f|
+          return JSON.parse(f.read, symbolize_names: true)
+        end
+      end
+
+      def set_cookie
+        tmp_cookie = load_cookie
+        return if tmp_cookie.blank?
+        session.visit(host)
+        tmp_cookie.each do |cookie|
+          session.driver.browser.manage.add_cookie(name: cookie[:name], value: cookie[:value], path: cookie[:path])
+        end
+      end
+
+      def save_cookie
+        tmp_cookies = session.driver.browser.manage.all_cookies
+        File.open(cookie_file_name, "w") do |f|
+          JSON.dump(tmp_cookies, f)
+        end
       end
   end
 end
