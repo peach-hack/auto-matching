49c5803cedc8b50969b17c0d853fad1fd7f7c5c3
tsu-nera <fox10225fox@gmail.com>
2019-01-10 23:32:46 +0900

    fix error

--- auto-matching-api-rails/lib/auto_matching/common/mint.rb
+++ auto-matching-api-rails/lib/auto_matching/common/mint.rb
@@ -9,7 +9,7 @@ module AutoMatching
         logging_start(__method__)
         session.fill_in "loginid", with: login_user
         session.fill_in "pwd", with: login_password
-        session.find("#B1login").click
+        session.find("#B1login").native.send_keys :enter
         logging_end(__method__)
       end
     end
