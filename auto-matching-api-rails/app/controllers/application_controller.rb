5c0df696926af218c0c05b8e50bbc97027ea0afb
tsu-nera <fox10225fox@gmail.com>
2019-01-12 00:23:03 +0900

    Rails API Modeへの移行

--- auto-matching-api-rails/app/controllers/application_controller.rb
+++ auto-matching-api-rails/app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
-class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-
+class ApplicationController < ActionController::API
   # 200 Success
   def response_success(class_name, action_name)
     render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
