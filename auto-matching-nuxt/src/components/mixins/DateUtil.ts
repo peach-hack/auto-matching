a27b56e2d7f4ea3bb19e14d19a0a92d555a8b267
tsu-nera <fox10225fox@gmail.com>
2019-01-11 19:58:58 +0900

    eslint fix

--- auto-matching-nuxt/src/components/mixins/DateUtil.ts
+++ auto-matching-nuxt/src/components/mixins/DateUtil.ts
@@ -2,15 +2,15 @@ import moment from 'moment'
 
 const DateUtil = {
   filters: {
-    showDate: function (value: string): string {
+    showDate: function(value: string): string {
       if (value === null) {
-        return "-"
+        return '-'
       } else {
         const date = moment(value)
         return date.format('YYYY-MM-DD HH:mm')
       }
     }
-  },
+  }
 }
 
 export default DateUtil
