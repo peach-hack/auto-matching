3a0425348ee307377ee2ce24962a4c07dc1be0ce
tsu-nera <fox10225fox@gmail.com>
2019-01-13 14:03:09 +0900

    annotate

--- auto-matching-api-rails/app/serializers/post_template_serializer.rb
+++ auto-matching-api-rails/app/serializers/post_template_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: post_templates
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :text             not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class PostTemplateSerializer < ApplicationSerializer
   attributes :id, :title, :body, :created_at, :updated_at
 end
