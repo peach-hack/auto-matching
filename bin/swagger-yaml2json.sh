13a585691ba4a6ae1fff5ff27f96f1eeae45b192
tsu-nera <fox10225fox@gmail.com>
2019-01-18 16:47:33 +0900

    add script

--- bin/swagger-yaml2json.sh
+++ bin/swagger-yaml2json.sh
@@ -5,3 +5,8 @@ docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate \
     -o ./local/tmp
 
 cp tmp/swagger.json swagger.json
+
+node auto-matching-nuxt/src/plugins/swagger-codegen.js
+
+sed -i'' -e 's/let domain = '\'''\''/let domaim = process.env.baseUrl/g' auto-matching-nuxt/src/plugins/api.js
+rm auto-matching-nuxt/src/plugins/api.js-e
