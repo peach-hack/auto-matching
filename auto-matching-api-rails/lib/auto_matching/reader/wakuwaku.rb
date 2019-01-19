c5052e65930667d1f2082327f282f2bf14a88c46
goi <kakedashiit@gmail.com>
2019-01-19 20:17:19 +0900

    search_board commit

--- auto-matching-api-rails/lib/auto_matching/reader/wakuwaku.rb
+++ auto-matching-api-rails/lib/auto_matching/reader/wakuwaku.rb
@@ -4,7 +4,26 @@ module AutoMatching
       include Common::Wakuwaku
 
       private
+        def search_board
+          logger.debug("search_board now")
+          # TODO
+          # 大人の募集 ※直リンクでいけた
+          session.visit "https://550909.com/m/bbs/genre?menu=adult"
+
+          # エリア選択　関東 > 東京都 > 新宿区　※直リンクでいけた
+          session.visit "https://550909.com/m/setting/set_city?mode=area&city=227&pref=14"
+
+          # すぐ会いたい ※直リンクでいけた
+          session.visit "https://550909.com/m/bbs/list?genre=3"
+        end
+
         def read_board
+          logger.debug("read_board now")
+          # TODO
+        end
+
+        def save_board
+          logger.debug("save_board now")
           # TODO
         end
     end
