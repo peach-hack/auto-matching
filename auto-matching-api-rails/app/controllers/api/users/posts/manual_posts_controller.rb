71a08a7741d65e25ced03a7f7a9708b9b986e44f
Toki <tokidrill@gmail.com>
2019-01-21 02:50:53 +0900

    連投防止論理の追加 #100 規定の時間(仮に3時間)以内に連投があったとき、投稿の試行を行わない。 もしかして、jobの中で試行するか否かを評価したほうがいい？

--- auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
+++ auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
@@ -11,6 +11,8 @@ module Api
           site_ids = params[:ids]
           debug_flag = params[:debug]
 
+          histories = SourceSite::ManualPostHistory.find(site_ids)
+
           sender_classes = [
             AutoMatching::Sender::Happymail,
             AutoMatching::Sender::Wakuwaku,
@@ -21,8 +23,16 @@ module Api
           ]
 
           site_ids.each do |id|
-            sender_class = sender_classes[id - 1].to_s
-            ManualPostJob.perform_later(debug_flag, sender_class)
+            site_history = histories.first
+
+            # 3時間以内の再投稿は試行せずに投稿禁止の旨を表示する
+            if (Time.zone.now - site_history.last_post_at) / 60 < 180 then
+              # TODO: フロント側「投稿ステータス」の部分にその旨を示すためにはどうすればよい？
+              logger.debug("連投検知")
+            else
+              sender_class = sender_classes[id - 1].to_s
+              ManualPostJob.perform_later(debug_flag, sender_class)
+            end
           end
 
           response_success(:manual_posts, :execute)
