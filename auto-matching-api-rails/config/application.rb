a04d739d82d942fb781fcd78b8cda83fa8f922ee
tsu-nera <fox10225fox@gmail.com>
2019-01-25 18:16:21 +0900

    modify

--- auto-matching-api-rails/config/application.rb
+++ auto-matching-api-rails/config/application.rb
@@ -49,8 +49,7 @@ module AutoMatching
     config.middleware.use ActionDispatch::Flash
 
     config.action_dispatch.default_headers = {
-      "Access-Control-Allow-Credentials" => "true",
-      "Access-Control-Allow-Origin" => "*",
+      "Access-Control-Allow-Origin" => "https://*.netlify.com",
       "Access-Control-Request-Method" => "*"
     }
   end
