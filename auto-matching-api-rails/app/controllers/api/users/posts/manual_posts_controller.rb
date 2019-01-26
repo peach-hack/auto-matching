7e76709781a37ed5361cee9cd2ffc815b749779f
Toki <tokidrill@gmail.com>
2019-01-27 05:11:41 +0900

    連投防止論理の追加 #100 最終投稿日時からの経過時間の評価をメソッドに抽出。 経過時間による分岐のfixとリファクタリング。 投稿を許容しない場合の制御は未実装。

--- auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
+++ auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
@@ -27,18 +27,23 @@ module Api
           site_ids.each do |id|
             site_history = histories.first
 
-            # 3時間以内の再投稿は試行せずに投稿禁止の旨を表示する
-            if (Time.zone.now - site_history.last_post_at) / 60 < 180 then
-              # TODO: フロント側「投稿ステータス」の部分にその旨を示すためにはどうすればよい？
-              logger.debug("連投検知")
-            else
+            if elapsed_time(site_history.last_post_at) > ENV["NEST_POST_ALLOW_TIME"].to_f
               sender_class = sender_classes[id - 1].to_s
               ManualPostJob.perform_later(debug_flag, sender_class)
+            else
+              # どうやって制御しよう。
             end
           end
 
           response_success(:manual_posts, :execute)
         end
+
+        private
+
+          def elapsed_time(last_time)
+            elapsed_seconds = Time.zone.now - last_time
+            elapsed_mitutes = elapsed_seconds / 60
+          end
       end
     end
   end
