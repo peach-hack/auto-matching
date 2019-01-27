501c8c7429727df0bd7a088fd31362fc4d482da4
Toki <tokidrill@gmail.com>
2019-01-27 19:34:03 +0900

    連投防止論理の追加 #100 複数サイトに一括してマニュアル投稿する場合に対応できていなかったので修正。 変数名などを再考し、修正。 allow_manual_post?の条件を強めるのは未実装。

--- auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
+++ auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
@@ -13,7 +13,7 @@ module Api
           site_ids = params[:ids]
           debug_flag = params[:debug]
 
-          histories = SourceSite::ManualPostHistory.find(site_ids)
+          manual_post_histories = SourceSite::ManualPostHistory.find(site_ids)
 
           sender_classes = [
             AutoMatching::Sender::Happymail,
@@ -25,11 +25,11 @@ module Api
           ]
 
           site_ids.each do |id|
-            @last_time = histories.first.last_post_at
+            @last_time = manual_post_histories.shift.last_post_at
 
             # MEMO: 連投制限に引っかかった場合は、Jobの呼び出しをスキップする。
             #       bad_requestを返さないので投稿ステータスが「実行中」から変わらないが、第一リリース時点では運用回避に留める。
-            # TODO: エラー返却の制御
+            # TODO: エラー返却の制御→第二リリース?
             next if !allow_manual_post?
 
             sender_class = sender_classes[id - 1].to_s
@@ -42,6 +42,7 @@ module Api
         private
 
           def allow_manual_post?
+            # TODO: 投稿制限条件を強める考慮
             calculate_elapsed_minutes > ENV["NEXT_MANUAL_POST_ALLOW_MINUTES"].to_f
           end
 
