d4225b7ebbea41a6827ca43a193247ce72c23b4a
goi <kakedashiit@gmail.com>
2019-01-27 18:29:13 +0900

    wip

--- auto-matching-api-rails/lib/auto_matching/converter/converter_base.rb
+++ auto-matching-api-rails/lib/auto_matching/converter/converter_base.rb
@@ -1,6 +1,10 @@
 module AutoMatching
   module Converter
     class ConverterBase
+      FROM_PREFECTURE_REGEX = /(...??[都道府県])/
+      FROM_CITY_REGEX = /(...??[都道府県])(...??[市区町村])/
+      FROM_ALL_REGEX = /(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])(.+)/
+
       def convert_to_generation(age)
         if age < 20
           "18-19歳"
@@ -31,6 +35,34 @@ module AutoMatching
         else
           "60歳以上"
         end
+      end
+
+      def convert_to_split_from(from)
+        if from.match(FROM_ALL_REGEX)
+          from.match(FROM_ALL_REGEX).captures
+        elsif from.match(FROM_CITY_REGEX)
+          from.match(FROM_CITY_REGEX).captures
+        else
+          from.match(FROM_PREFECTURE_REGEX).captures
+        end
+      end
+
+      def convert_to_from(from_list)
+        from_length = from_list.length
+        if from_length == 3
+          prefecture = from_list[0]
+          city = from_list[1]
+          address = from_list[2]
+        elsif from_length == 2
+          prefecture = from_list[0]
+          city = from_list[1]
+          address = ""
+        elsif from_length == 1
+          prefecture = from_list[0]
+          city = ""
+          address = ""
+        end
+        [prefecture, city, address]
       end
     end
   end
--- auto-matching-api-rails/lib/auto_matching/converter/pcmax.rb
+++ auto-matching-api-rails/lib/auto_matching/converter/pcmax.rb
@@ -1,13 +1,12 @@
 module AutoMatching
   module Converter
     class Pcmax < ConverterBase
-      FROM_PREFECTURE_REGEX = /(...??[都道府県])/
-      FROM_CITY_REGEX = /(...??[都道府県])(...??[市区町村])/
-      FROM_ALL_REGEX = /(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])(.+)/
 
       # valueには性別、名前、年齢が格納されているためそれぞれ分割
       def split_value(value)
-        sex_list = name_list = age_list = []
+        sex_list = []
+        name_list = []
+        age_list = []
 
         value.each do |v|
           sex, name, age = split_sex_name_age(v)
@@ -47,64 +46,32 @@ module AutoMatching
       # post_fromは住所を丸々格納しているのでそれぞれ分割
       # 例：神奈川県川崎市中原区 → 神奈川県、川崎市、中原区
       def value_change_from(post_from)
-        from = []
+        from_list = []
 
-        post_from.each do |args|
-          unless args.include?("都")
-            unless args.include?("県")
-              if args.size = 2
-                args << "県"
-              else
-                args.insert(2, "県")
-              end
-            end
-            if args.include?("北海道")
-              # 何もしない
-            elsif args.include?("東京")
-              args.insert(2, "都")
-            end
-          end
-
-          if args.match(FROM_PREFECTURE_REGEX)
-            from.push(args.match(FROM_PREFECTURE_REGEX).captures)
-          elsif args.match(FROM_CITY_REGEX)
-            from.push(args.match(FROM_CITY_REGEX).captures)
-          else
-            from.push(args.match(FROM_ALL_REGEX).captures)
-          end
+        post_from.each do |v|
+          from = convert_to_split_from(v)
+          from_list.push(from)
         end
 
-        from
+        from_list
       end
 
       # 分割された住所を確変数に割り当てる
-      def split_from(from)
-        prefecture = []
-        city = []
-        address = []
+      def split_post_from(from)
+        prefecture_list = []
+        city_list = []
+        address_list = []
 
         from.each_with_index do |v, i|
-          number_of_fromdata = v.length
-          if number_of_fromdata == 3
-            add_prefecture = v[0]
-            add_city = v[1]
-            add_address = v[2]
-          elsif number_of_fromdata == 2
-            add_prefecture = v[0]
-            add_city = v[1]
-            add_address = ""
-          elsif number_of_fromdata == 1
-            add_prefecture = v[0]
-            add_city = ""
-            add_address = ""
-          end
 
-          prefecture.push(add_prefecture.to_s.strip)
-          city.push(add_city.to_s.strip)
-          address.push(add_address.to_s.strip)
+          prefecture, city, address = convert_to_from(v)
+
+          prefecture_list.push(prefecture.to_s.strip)
+          city_list.push(city.to_s.strip)
+          address_list.push(address.to_s.strip)
         end
 
-        return prefecture, city, address
+        [prefecture_list, city_list, address_list]
       end
     end
   end
