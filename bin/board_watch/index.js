31a57295816de6472ed2bd32e81b03964ea33a90
tsu-nera <fox10225fox@gmail.com>
2019-02-02 01:48:50 +0900

    name change

--- bin/board_watch/index.js
+++ bin/board_watch/index.js
@@ -1,14 +1,9 @@
 const exec = require('child_process').exec;
 
-module.exports.board_watch = function () {
-  exec('./board_watch.sh', function (err, stdout, stderr) {
+exports.boardWatch = (request, response) => {
+  exec('./script.sh', function (err, stdout, stderr) {
     if (err) { console.log(err); }
   });
-}
-
-module.exports.daily_post = function () {
-  exec('./daily_post.sh', function (err, stdout, stderr) {
-    if (err) { console.log(err); }
-  });
-}
+  response.status(200).end();
+};
 
