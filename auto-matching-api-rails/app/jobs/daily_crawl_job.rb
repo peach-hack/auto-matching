cf541c8ea3dd12d703fd3ad0b47f4d522d1596a0
tsu-nera <fox10225fox@gmail.com>
2019-02-01 21:48:20 +0900

    typo

--- auto-matching-api-rails/app/jobs/daily_crawl_job.rb
+++ auto-matching-api-rails/app/jobs/daily_crawl_job.rb
@@ -1,5 +1,5 @@
 class DailyCrawlJob < AutoJob
-  def perform(debug_flag, sender_class_s)
+  def perform(sender_class_s)
     klass = sender_class_s.constantize
 
     AutoMatching::Reader::Executor.new.run(klass)
