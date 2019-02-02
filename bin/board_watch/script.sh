76cc152e4eb35c448e2bd7b1eb2158f00e6ad416
tsu-nera <fox10225fox@gmail.com>
2019-02-02 23:23:33 +0900

    update cookie

--- bin/board_watch/script.sh
+++ bin/board_watch/script.sh
@@ -2,8 +2,7 @@
 
 curl -H "Accept: application/json" -H "Content-Type: application/json" \
      -X POST -d "{ \"session\": {\"username\": \"${USERNAME}\", \"password\": \"${PASSWORD}\"}}" \
-     $URL/api/session -w '\n%{http_code}\n' -c cookie -s
-
-curl -X POST -H 'Content-Type:application/json' \
+     $URL/api/session -w '\n%{http_code}\n' -s -c - | \
+curl -b @- -X POST -H 'Content-Type:application/json' \
      -d "{\"ids\": [2,3,4]}" \
-     $URL/api/users/posts/daily-crawl -w '\n%{http_code}\n' -s -b cookie
+     $URL/api/users/posts/daily-crawl -w '\n%{http_code}\n' -s
