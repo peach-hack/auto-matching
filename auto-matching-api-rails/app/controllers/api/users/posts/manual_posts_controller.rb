bcb65baf27678233b9ee5cb26e6f6717d0609fb6
Toki <tokidrill@gmail.com>
2019-02-05 23:23:12 +0900

    連続投稿防止論理の追加 #100 issue331の結果を受けて、job側で抑止論理を追加。 抑止後のハンドリングは未定なので、想定しているハンドリングを記述してある。

--- auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
+++ auto-matching-api-rails/app/controllers/api/users/posts/manual_posts_controller.rb
@@ -13,8 +13,6 @@ module Api
           site_ids = params[:ids]
           debug_flag = params[:debug]
 
-          manual_post_histories = SourceSite::ManualPostHistory.find(site_ids)
-
           sender_classes = [
             AutoMatching::Sender::Happymail,
             AutoMatching::Sender::Wakuwaku,
@@ -25,31 +23,12 @@ module Api
           ]
 
           site_ids.each do |id|
-            @last_time = manual_post_histories.shift.last_post_at
-
-            # MEMO: 連投制限に引っかかった場合は、Jobの呼び出しをスキップする。
-            #       bad_requestを返さないので投稿ステータスが「実行中」から変わらないが、第一リリース時点では運用回避に留める。
-            # TODO: エラー返却の制御→第二リリース?
-            next if !allow_manual_post?
-
             sender_class = sender_classes[id - 1].to_s
             ManualPostJob.perform_later(debug_flag, sender_class)
           end
 
           response_success(:manual_posts, :execute)
         end
-
-        private
-
-          def allow_manual_post?
-            # TODO: 投稿制限条件を強める考慮
-            calculate_elapsed_minutes > ENV["NEXT_MANUAL_POST_ALLOW_MINUTES"].to_f
-          end
-
-          def calculate_elapsed_minutes
-            elapsed_seconds = Time.zone.now - @last_time
-            elapsed_seconds / 60
-          end
       end
     end
   end
--- auto-matching-api-rails/app/jobs/manual_post_job.rb
+++ auto-matching-api-rails/app/jobs/manual_post_job.rb
@@ -3,17 +3,26 @@ class ManualPostJob < ApplicationJob
 
   SUCCESS = "成功".freeze
   ERROR = "失敗".freeze
+  SUSPEND = "抑止".freeze
 
   def perform(debug_flag, sender_class_s)
     $DEBUG = debug_flag
     klass = sender_class_s.constantize
     @key = klass.new.source_site_key
 
-    AutoMatching::Sender::Executor.new.run(klass)
+    history = SourceSite::ManualPostHistory.find_by(key: @key)
+
+    if allow_manual_post?(history)
+      AutoMatching::Sender::Executor.new.run(klass)
+    else
+      raise hogehoge
+    end
 
     set_success
   rescue StandardError
     set_error
+    rescue hogehoge
+      set_suspend
   ensure
     $DEBUG = false
   end
@@ -31,7 +40,20 @@ class ManualPostJob < ApplicationJob
       ActionCable.server.broadcast "manual_post_channel", ids: [history.id], status: ERROR
     end
 
+    def set_suspend
+      ActionCable.server.broadcast "manual_post_channel", ids: [history.id], status: SUSPEND
+    end
+
     def date
       Time.zone.now
+    end
+
+    def allow_manual_post?(history)
+      calculate_elapsed_minutes(history) > ENV["NEXT_MANUAL_POST_ALLOW_MINUTES"].to_f && history.last_post_status != "成功"
+    end
+
+    def calculate_elapsed_minutes(history)
+      elapsed_seconds = date - history.last_post_at
+      elapsed_seconds / 60
     end
 end
