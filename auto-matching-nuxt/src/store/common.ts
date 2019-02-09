926a555e0e85e75fdfcf2fe48fc0e85ac4ad2c33
tsu-nera <fox10225fox@gmail.com>
2019-02-09 20:21:26 +0900

    ローディング処理

---
+++ auto-matching-nuxt/src/store/common.ts
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex, { Mutation } from 'vuex'
+
+Vue.use(Vuex)
+
+export interface State {
+  isLoading: boolean
+}
+
+export interface Context {
+  commit: (name: string, palload?: any) => void
+}
+
+export interface Mutations {
+  [key: string]: (state: State, payload?: any) => void
+}
+
+export const state: () => State = () => ({
+  isLoading: false
+})
+
+export const mutations: Mutations = {
+  loading(state, isLoading) {
+    state.isLoading = isLoading;
+  }
+}
+
+export default {
+  state,
+  mutations,
+}
