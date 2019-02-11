4c17d1ab95fde8841cefe754991626a89bd81678
goi <kakedashiit@gmail.com>
2019-02-11 23:35:42 +0900

    Refactoring

--- auto-matching-api-rails/lib/auto_matching/capacity_control.rb
+++ auto-matching-api-rails/lib/auto_matching/capacity_control.rb
@@ -1,13 +1,32 @@
 module AutoMatching
   module CapacityControl
     def delete_extra_posts
-      logger.debug("--- Post Table Count ---")
+      puts "--- Post Table Count ---"
       capacity = Post.count
-      logger.debug("Post DB Count ---> #{capacity}")
+      puts "Post DB Count ---> #{capacity}"
 
       if capacity > 7000
-        posts = Post.where('post_at < ?', 7.day.ago)
-        posts.delete_all
+        all_delete = 0
+        only_delete = 0
+        ago_post = Post.where('post_at < ?', 1.week.ago)
+        posts_before_7days_ago = ago_post.count
+        ago_post.each do |b|
+          begin
+            if Post.where(profile_id: b.profile_id).count == 1
+              all_delete += 1
+              Profile.find(b.profile_id).destroy
+            else
+              only_delete += 1
+              b.delete
+            end
+          rescue => e
+            puts "--ActiveRecord::RecordNotFound--"
+            puts e
+          end
+        end
+        puts "Posts before 7 days ago -------------------> #{posts_before_7days_ago}"
+        puts "Number of all(Profile & Post) deletions ---> #{all_delete}"
+        puts "Number of only(Post) deletions ------------> #{only_delete}"
       end
     end
   end
