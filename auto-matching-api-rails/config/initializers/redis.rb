fa56b5342b5d9e9a85ff85b31218f370af68a1bc
tsu-nera <fox10225fox@gmail.com>
2019-02-11 21:37:42 +0900

    update

--- auto-matching-api-rails/config/initializers/redis.rb
+++ auto-matching-api-rails/config/initializers/redis.rb
@@ -1,2 +1,5 @@
 REDIS ||= Redis.new(url: ENV["REDIS_URL"] || "redis://localhost:6379") if Rails.env.development?
-REDIS ||= Redis.new(url: ENV["REDISTOGO_URL"] || "redis://localhost:6379") if Rails.env.production?
+if Rails.env.production?
+  uri = URI.parse(ENV.fetch("REDISTOGO_URL", "redis://localhost:6379/"))
+  REDIS ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
