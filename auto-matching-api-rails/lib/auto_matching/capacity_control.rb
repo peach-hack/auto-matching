67efbe689c281bc6011864701c5ac5660cf6f975
tsu-nera <fox10225fox@gmail.com>
2019-02-12 12:47:01 +0900

    rubocop 指摘対応

--- auto-matching-api-rails/lib/auto_matching/capacity_control.rb
+++ auto-matching-api-rails/lib/auto_matching/capacity_control.rb
@@ -8,21 +8,19 @@ module AutoMatching
       if capacity > 7000
         all_delete = 0
         only_delete = 0
-        ago_post = Post.where('post_at < ?', 1.week.ago)
+        ago_post = Post.where("post_at < ?", 1.week.ago)
         posts_before_7days_ago = ago_post.count
         ago_post.each do |b|
-          begin
-            if Post.where(profile_id: b.profile_id).count == 1
-              all_delete += 1
-              Profile.find(b.profile_id).destroy
-            else
-              only_delete += 1
-              b.delete
-            end
-          rescue => e
-            puts "--ActiveRecord::RecordNotFound--"
-            puts e
+          if Post.where(profile_id: b.profile_id).count == 1
+            all_delete += 1
+            Profile.find(b.profile_id).destroy
+          else
+            only_delete += 1
+            b.delete
           end
+        rescue => e
+          puts "--ActiveRecord::RecordNotFound--"
+          puts e
         end
         puts "Posts before 7 days ago -------------------> #{posts_before_7days_ago}"
         puts "Number of all(Profile & Post) deletions ---> #{all_delete}"
