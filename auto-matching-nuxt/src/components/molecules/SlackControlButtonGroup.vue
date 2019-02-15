45360b893cb681083dcee7dcebe82288a4440c48
tsu-nera <fox10225fox@gmail.com>
2019-02-15 15:46:15 +0900

    簡単なタイマー

--- auto-matching-nuxt/src/components/molecules/SlackControlButtonGroup.vue
+++ auto-matching-nuxt/src/components/molecules/SlackControlButtonGroup.vue
@@ -2,14 +2,29 @@
 .form-group
   label Slackリアルタイム通知
   .button-group
-    button(type="button").btn.btn-primary
+    button(type="button" @click="start").btn.btn-primary
       | Start
-    button(type="button").btn.btn-light
+    button(type="button" @click="stop").btn.btn-light
       | Stop
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  methods: {
+    start: function() {
+      console.log('slack notify start')
+      this.timerObj = setInterval(function() {
+        console.log('slack notify exec.')
+      }, 1000)
+      this.timerOn = true
+    },
+    stop: function() {
+      clearInterval(this.timerObj)
+      this.timerOn = false
+      console.log('slack notify stop')
+    }
+  }
+})
 </script>
