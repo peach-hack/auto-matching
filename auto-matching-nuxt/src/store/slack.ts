c054421b943f9ead037b0263f3cac83d394eb88b
tsu-nera <fox10225fox@gmail.com>
2019-02-15 22:12:17 +0900

    wip

---
+++ auto-matching-nuxt/src/store/slack.ts
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import * as Api from '../plugins/api'
+
+Vue.use(Vuex)
+
+export interface State {
+  timerOn: boolean
+  timerObj: object
+}
+
+export interface Context {
+  commit: (name: string, palload?: any) => void
+}
+
+export interface Mutations {
+  [key: string]: (state: State, payload?: any) => void
+}
+
+export interface Actions {
+  [key: string]: (context: Context, payload?: any) => void
+}
+
+export const state: () => State = () => ({
+  timerOn: false,
+  timerObj: null
+})
+
+export const mutations: Mutations = {
+  changeTimerOn(state: State, timerOn: boolean) {
+    state.timerOn = timerOn
+  },
+  changeTimerObj(state: State, timerObj: object) {
+    state.timerObj = timerObj
+  }
+}
+
+export const actions: Actions = {
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
