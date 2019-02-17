32d3b506ad2f87d38cd53b0df4942d7e60f18cb4
tsu-nera <fox10225fox@gmail.com>
2019-02-17 11:29:19 +0900

    wip

--- auto-matching-nuxt/src/components/organisms/SchedulerForm.vue
+++ auto-matching-nuxt/src/components/organisms/SchedulerForm.vue
@@ -1,9 +1,52 @@
 <template lang="pug">
-div
+table.table
+  thead
+    tr
+      th Id 
+      th サイト名
+      th 定期投稿
+      th 定期監視
+  tbody
+    tr(v-for="site in sites" :key="site.id")
+      td {{ site.id }}
+      td {{ site.name }}
+      td
+      td
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data: function() {
+    return {
+      sites: [
+        {
+          id: 1,
+          name: 'ハッピーメール'
+        },
+        {
+          id: 2,
+          name: 'ワクワクメール'
+        },
+        {
+          id: 3,
+          name: 'PCMAX'
+        },
+        {
+          id: 4,
+          name: 'イクヨクルヨ'
+        },
+        {
+          id: 5,
+          name: 'ミントC!Jメール'
+        },
+        {
+          id: 6,
+          name: 'メルパラ'
+        }
+      ]
+    }
+  }
+})
 </script>
