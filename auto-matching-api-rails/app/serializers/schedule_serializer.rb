9c8bf7aba8fc8259adc9ed4fe668cc17830e5ba0
tsu-nera <fox10225fox@gmail.com>
2019-02-17 20:58:22 +0900

    schema定義

---
+++ auto-matching-api-rails/app/serializers/schedule_serializer.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: schedules
+#
+#  id          :bigint(8)        not null, primary key
+#  post_cron   :string
+#  post_sites  :string
+#  watch_cron  :string
+#  watch_sites :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
