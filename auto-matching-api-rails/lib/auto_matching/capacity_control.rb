87a569f5baa69d5b5eb6c0f4ba99f60e6ac3f5f9
goi <kakedashiit@gmail.com>
2019-02-17 17:52:33 +0900

    wip

--- auto-matching-api-rails/lib/auto_matching/capacity_control.rb
+++ auto-matching-api-rails/lib/auto_matching/capacity_control.rb
@@ -5,26 +5,59 @@ module AutoMatching
       capacity = Post.count
       puts "Post DB Count ---> #{capacity}"
 
-      if capacity > 7000
-        all_delete = 0
-        only_delete = 0
+      if true # capacity > 7000
+        all_delete_no = 0
+        only_delete_no = 0
+        all_delete_list = []
+        only_delete_list = []
         ago_post = Post.where("post_at < ?", 1.week.ago)
         posts_before_7days_ago = ago_post.count
         ago_post.each do |b|
-          if Post.where(profile_id: b.profile_id).count == 1
-            all_delete += 1
-            Profile.find(b.profile_id).destroy
-          else
-            only_delete += 1
-            b.delete
+          if Post.where(profile_id: b.profile_id).nil?
+            # 何もしない
+          elsif Post.where(profile_id: b.profile_id).count == 1
+            all_delete_no += 1
+            all_delete_list.push(Profile.find(b.profile_id).id)
+            # Profile.find(b.profile_id).destroy => 前の処理のため一旦コメントアウト
+          elsif Post.where(profile_id: b.profile_id).count > 1
+            only_delete_no += 1
+            only_delete_list.push(b.id)
+            # b.delete => 前の処理のため一旦コメントアウト
           end
-        rescue => e
-          puts "--ActiveRecord::RecordNotFound--"
-          puts e
         end
+        all_delete_id = ""
+        only_delete_id = ""
+        all_delete_list.map { |v| all_delete_id << v.to_s + "," }
+        only_delete_list.map { |v| only_delete_id << v.to_s + "," }
+        all_delete_id.chop!
+        only_delete_id.chop!
+
+        puts "\n\n"
+        puts "-- all_delete_list --"
+        puts all_delete_list
+        puts "-- only_delete_list --"
+        puts only_delete_list
+        puts "-- all_delete_id --"
+        puts all_delete_id
+        puts "-- only_delete_id --"
+        puts only_delete_id
+        puts "\n\n"
+        all_delete_profile_sql = "DELETE FROM PROFILES WHERE ID IN (#{all_delete_id})"
+        all_delete_post_sql = "DELETE FROM POSTS WHERE PROFILE_ID IN (#{all_delete_id})"
+        only_delete_post_sql = "DELETE FROM POSTS WHERE ID IN (#{only_delete_id})"
+
+        if all_delete_list
+          Profile.find_by_sql(all_delete_profile_sql)
+          Post.find_by_sql(all_delete_post_sql)
+        end
+
+        Post.find_by_sql(only_delete_post_sql) if only_delete_id
+
+        # Profile.destroy(all_delete_list) if all_delete_list => 前の処理のため一旦コメントアウト
+        # Post.delete(only_delete_list) if only_delete_list => 前の処理のため一旦コメントアウト
         puts "Posts before 7 days ago -------------------> #{posts_before_7days_ago}"
-        puts "Number of all(Profile & Post) deletions ---> #{all_delete}"
-        puts "Number of only(Post) deletions ------------> #{only_delete}"
+        puts "Number of all(Profile & Post) deletions ---> #{all_delete_no}"
+        puts "Number of only(Post) deletions ------------> #{only_delete_no}"
       end
     end
   end
