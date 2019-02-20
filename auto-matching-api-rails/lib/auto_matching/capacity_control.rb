32616a323b18b5daa0f14587b6bf5ea26b623ecd
goi <kakedashiit@gmail.com>
2019-02-20 23:17:33 +0900

    ActiveRecord

--- auto-matching-api-rails/lib/auto_matching/capacity_control.rb
+++ auto-matching-api-rails/lib/auto_matching/capacity_control.rb
@@ -12,6 +12,7 @@ module AutoMatching
         only_delete_list = []
         ago_post = Post.where("post_at < ?", 1.week.ago)
         posts_before_7days_ago = ago_post.count
+
         ago_post.each do |b|
           post_count = Post.where(profile_id: b.profile_id).count
           if post_count == 0
@@ -25,19 +26,13 @@ module AutoMatching
           end
         end
 
-        all_delete_id = all_delete_list.join(",").chop!
-        only_delete_id = only_delete_list.join(",").chop!
-
-        all_delete_profile_sql = "DELETE FROM PROFILES WHERE ID IN (#{all_delete_id})"
-        all_delete_post_sql = "DELETE FROM POSTS WHERE PROFILE_ID IN (#{all_delete_id})"
-        only_delete_post_sql = "DELETE FROM POSTS WHERE ID IN (#{only_delete_id})"
-
         if all_delete_list.present?
-          Profile.find_by_sql(all_delete_profile_sql)
-          Post.find_by_sql(all_delete_post_sql)
+          Profile.destroy(all_delete_list)
         end
 
-        Post.find_by_sql(only_delete_post_sql) if only_delete_id.present?
+        if only_delete_list.present?
+          Post.destroy(only_delete_list)
+        end
 
         puts "Posts before 7 days ago -------------------> #{posts_before_7days_ago}"
         puts "Number of all(Profile & Post) deletions ---> #{all_delete_no}"
