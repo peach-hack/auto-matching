6b9e3175e59193391f9ee85315ed37e350b9028a
tsu-nera <fox10225fox@gmail.com>
2019-03-13 03:35:07 +0900

    discription変更

--- auto-matching-docs/website/siteConfig.js
+++ auto-matching-docs/website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'Auto Matching', // Title for your website.
-  tagline: 'Integrated Deai Engine',
+  tagline: '統合出会い系エンジン',
   url: 'https://auto-matching.netlify.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
